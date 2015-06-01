0aae213deb80896294d68373715a5c9742e4c58d
Sam <ghoshbandana1@gmail.com>
2015-06-01 10:37:59 +0530

    minor changes

--- src/com/algos/fk/SomeProbs.java
+++ src/com/algos/fk/SomeProbs.java
@@ -3,6 +3,11 @@
  */
 package com.algos.fk;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author sam
  *
@@ -14,7 +19,12 @@ public class SomeProbs {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		findCeleb();
+		//findCeleb();
+		
+		char[] pat = "ABC".toCharArray();
+		char[] str = "ABDABBC"
+		
+		searchMinWindow
 	}
 
 	/**
@@ -73,4 +83,119 @@ public class SomeProbs {
 		else
 			return low;
 	}
+	
+	/**
+	 * Input :
+	 * 
+	 * List of edges are given in the format (source,destination) –> (s1,d1)
+	 * (s2,d2)
+	 * 
+	 * There are some error codes with priority.
+	 * 
+	 * 1 – Loop
+	 * 
+	 * 2 – Multiple roots
+	 * 
+	 * 3 – More than two children
+	 * 
+	 * Output :
+	 * 
+	 * Indicate the error (considering the priority) in case of any error and
+	 * exit the program.
+	 * 
+	 * Print the tree structure in the bracket notation in case of no errors.
+	 * 
+	 * (e.g. (A(B(D)(E))(C(F)(G))) .. Here A is root. B and C are children of A.
+	 * D and E are children of B. F and G are children of C.)
+	 */
+	
+	public static void findError(ErrNode<Integer>[] links) {
+		Arrays.sort(links);
+		Map<Integer, Integer> roots = new HashMap<Integer, Integer>();
+		
+		for(ErrNode<Integer> node: links) {
+			
+			Integer dest = roots.get(node.dest);
+			
+			if(roots.get(node.dest) != null)
+				System.err.println(" loop is found");
+			
+			Integer root = roots.get(node.source);
+			
+			if(root == null) {
+				roots.put(node.source, 1);
+			} else {
+				roots.put(node.source, 2);
+			}
+			
+		}
+	}
+	
+	private static class ErrNode<E extends Comparable<E>> implements Comparator<ErrNode<E>>{
+		E source;
+		E dest;
+		
+		ErrNode(E source, E dest) {
+			this.source = source;
+			this.dest = dest;
+		}
+
+		@Override
+		public int compare(ErrNode<E> o1, ErrNode<E> o2) {
+			
+			return o1.source.compareTo(o2.source) == 0 ? o1.dest.compareTo(o2.dest) : o1.source.compareTo(o2.source);
+		}
+		
+	}
+	
+	/**
+	 * It was long description for a DNA problem. Main DNA sequence(a string) is
+	 * given (let say strDNA) and another string to search for(let say strPat).
+	 * You have to find the minimum length window in strDNA where strPat is
+	 * subsequence.
+	 */
+	
+	public static void searchMinWindow(char[] pat, char[] str) {
+		int chr [] = new int[26];
+		
+		for (char ch: pat) {
+			int index = ch - 'A';
+			chr [index]++;
+		}
+		for (int i= 0; i< chr.length; i++) {
+			if(chr [i]> 0)
+			 chr [i]++;
+		}
+		
+		int patlen = pat.length;
+		
+		int Mstart =0, Mend =str.length-1;
+		int lenFound =0;
+		for(int end = 0, start = 0 ; end < str.length; end++) {
+			
+			if(lenFound < patlen) {
+			if(chr[str[end]- 'A'] > 1) {
+				chr[str[end]- 'A']--;
+				lenFound++;
+			}
+			continue;
+			}
+			if(lenFound == patlen) {
+				while(chr[str[start] - 'A'] == 0) {
+					start++;
+				} 
+				if(end- start < Mend - Mstart){
+					Mstart = start;
+					Mend = end;
+				}
+				chr[str[start]- 'A']++;
+				lenFound--;
+			}
+			
+		}
+		System.out.println(String.format("Start: %d End:%d", Mstart, Mend));
+		
+	}
+	
+	
 }
